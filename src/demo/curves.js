02acd7ac2498d9405db6e249800492c75455db67
Ben Ellis <benjaminster@gmail.com>
2012-11-24 18:07:45 -0500

    Dicking around

--- src/demo/curves.js
+++ src/demo/curves.js
@@ -16,37 +16,104 @@ var subcanvas = new ble.scratch.Subcanvas(
     new goog.math.Box(0, canvas.width_px, canvas.height_px, 0),
     new goog.math.Box(size * yOverX, size, -size*yOverX, -size));
 
-var iter = 0;
-window.setInterval(function() {
-  var th = 2 * Math.PI * iter / 100;
-  var i0 = 100*(1 - Math.cos(th));
 
-  var theCurve = new ble.curves.Kappa(
-      new goog.math.Vec2(0, 0),
-      0,
-      []);
-  theCurve.curve = [];
-  theCurve.deltaAngle = 10 * ble.curves.DEGREE;
-  var segments = theCurve.curve;
-  var imax = 1000;
-  for(var i = 0; i < imax+i0; i++) { 
-    var sign = i % 3 == 0 ? 1 : -1;
-    var radius = 0.0015 * (i+i0);
-    var angle = Math.PI * sign / 3.5;
-    var inorm = i / imax;
-    var length = Math.abs(angle) * radius;
-    segments.push(new ble.curves.CurvedPart(length, sign / radius));
+
+canvas.withContext(function(ctx) {
+  ctx.clearRect(0, 0, this.width_px, this.height_px);
+});
+
+
+var pathOne = function(ctx, x, y, inverse, first, second, flip) {
+  var r = 1/2;
+  var s = flip ? -r : r;
+
+  if(inverse) {
+    ctx.moveTo(x - r, y - s);
+    ctx.lineTo(x - r, y + s);
+    ctx.lineTo(x + r, y + s);
+    ctx.lineTo(x + r, y - s);
+    ctx.lineTo(x - r, y - s);
   }
-  canvas.withContext(function(ctx) {
-    ctx.clearRect(0, 0, this.width_px, this.height_px);
-  });
-  subcanvas.withContext(function(ctx) {
-    ctx.save();
-    ctx.strokeStyle = "rgb(0,0,0)";
-    ctx.lineWidth *= 0.75;
-    theCurve.rendering = null;
-    theCurve.draw(ctx);
-    ctx.restore();
-  });
-  iter++;
-}, 10);
+
+  if(first) {
+    if(inverse)
+      ctx.lineTo(x - r, y - s);
+    else
+      ctx.moveTo(x - r, y - s);
+    ctx.arc(x, y - s, r, 0, (flip ? -1 : 1) * Math.PI / 2, flip);
+  }
+  if(second) {
+    if(first || inverse)
+      ctx.lineTo(x - r, y);
+    else
+      ctx.moveTo(x - r, y);
+    ctx.arc(x, y, r, 0, Math.PI, flip);
+  }
+  if(first) {
+    ctx.lineTo(x, y);
+    ctx.arc(x, y - s, r, (flip ? -1 : 1) * Math.PI / 2, Math.PI, flip);
+  }
+  /*
+  if(first) {
+    ctx.moveTo(x - r, y);
+    ctx.arc(x, y, r, 0, Math.PI);
+  }
+  if(second) {
+    ctx.lineTo(x - r, y - r);
+    ctx.arc(x, y - r, r, 0, Math.PI);
+  }
+  */
+};
+
+//ops = [pathSemiCircle, pathDoubleSemiCircle, pathInverseSemiCircle];
+
+subcanvas.withContext(function(ctx) {
+  ctx.save();
+  ctx.fillStyle = "rgb(255,0,0)";
+  ctx.beginPath();
+  pathOne(ctx, -2, 0, 0, 1, 1, false);
+  pathOne(ctx, -1, 0, 0, 0, 1, false);
+  pathOne(ctx, 0, 0, 0, 1, 0, false);
+  pathOne(ctx, -2, 1, 1, 1, 1, false);
+  pathOne(ctx, -1, 1, 1, 0, 1, false);
+  pathOne(ctx, 0, 1, 1, 1, 0, false);
+
+  pathOne(ctx, -2, 2, 0, 1, 1, true);
+  pathOne(ctx, -1, 2, 0, 0, 1, true);
+  pathOne(ctx, 0, 2, 0, 1, 0, true);
+
+  pathOne(ctx, -2, -1, 1, 1, 1, true);
+  pathOne(ctx, -1, -1, 1, 0, 1, true);
+  pathOne(ctx, 0, -1, 1, 1, 0, true);
+
+  ctx.fill();
+  ctx.restore();
+  return;
+  for(var x = -size; x <= size; x += 1) {
+    for(
+        var y = Math.round(-size*yOverX);
+        y <= Math.round(size*yOverX);
+        y+= 1) {
+      var op = Math.floor(3 * Math.random());
+      ops[op](ctx, x, y, 1);
+      /*
+      var parity = (Math.random() >= 0.5);
+      if(parity) {
+        ctx.moveTo(x + 0.5, y);
+        ctx.arc(x, y, 0.5, 0, Math.PI);
+        ctx.closePath();
+      } else {
+        ctx.moveTo(x + 0.5, y);
+        ctx.lineTo(x + 0.5, y+1);
+        ctx.lineTo(x - 0.5, y+1);
+        ctx.lineTo(x - 0.5, y);
+        ctx.arc(x, y, 0.5, Math.PI, 0, true);
+        ctx.closePath();
+      }
+      */
+    }
+  }
+
+      ctx.fill();
+  ctx.restore();
+});
