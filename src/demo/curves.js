6c39a72e1e0847ffb688d06d925f925aa8bca180
Ben Ellis <benjaminster@gmail.com>
2012-11-25 00:59:55 -0500

    more curvature junk

--- src/demo/curves.js
+++ src/demo/curves.js
@@ -9,7 +9,7 @@ goog.require('ble.curves.Kappa');
 var canvas = new ble.scratch.Canvas(1366, 768);
 canvas.render(document.body);
 
-var size = 3;
+var size = 10;
 var yOverX = canvas.height_px / canvas.width_px;
 var subcanvas = new ble.scratch.Subcanvas(
     canvas,
@@ -22,179 +22,27 @@ canvas.withContext(function(ctx) {
   ctx.clearRect(0, 0, this.width_px, this.height_px);
 });
 
-var pathInvertVerticalHalfCircle = function(ctx, x, y, r, move, flip, arc) {
-  if(move)
-    ctx.moveTo(x, y - r);
-  else
-    ctx.lineTo(x, y - r);
-  var s = flip ? r : -r;
-  ctx.lineTo(x - s, y - r);
-  ctx.lineTo(x - s, y + r);
-  ctx.lineTo(x,     y + r);
-  ctx.lineTo(x,     y - r);
-  if(arc)
-    ctx.arc(x, y, r, Math.PI / 2, -Math.PI / 2, !flip);
-  ctx.closePath();
-}
-
-
-var pathVerticalHalfCircle = function(ctx, x, y, r, move, flip) {
-  if(move)
-    ctx.moveTo(x, y - r);
-  else
-    ctx.lineTo(x, y - r);
-  ctx.arc(x, y, r, -Math.PI / 2, Math.PI / 2, flip);
-  ctx.closePath();
-}
-
-
-var pathInvertHorizontalHalfCircle = function(ctx, x, y, r, move, flip, arc) {
-  if(move)
-    ctx.moveTo(x - r, y);
-  var s = flip ? r : -r;
-  ctx.lineTo(x - r, y - s);
-  ctx.lineTo(x + r, y - s);
-  ctx.lineTo(x + r, y);
-  ctx.lineTo(x - r, y);
-  if(arc)
-    ctx.arc(x, y, r, 0, Math.PI, flip);
-  ctx.closePath();
-}
-
-var pathHorizontalHalfCircle = function(ctx, x, y, r, move, flip) {
-  if(move)
-    ctx.moveTo(x - r, y);
-  else
-    ctx.lineTo(x - r, y);
-  ctx.arc(x, y, r, 0, Math.PI, flip);
-  ctx.closePath();
-}
-
-var drawBlock = function(ctx, x, y, r, code) {
-  var semi1    = code & 1,
-      semi2    = code & 2,
-      vertical = code & 4,
-      flip     = code & 8,
-      invert   = code & 16;
-  var x1 = vertical  ? flip ? x + r : x - r : x;
-  var y1 = !vertical ? flip ? y + r : y - r : y;
-  var f;
-  if(!vertical && invert)
-    f = pathInvertHorizontalHalfCircle;
-  if(!vertical && !invert)
-    f = pathHorizontalHalfCircle;
-  if(vertical && invert)
-    f = pathInvertVerticalHalfCircle;
-  if(vertical && !invert)
-    f = pathVerticalHalfCircle;
-  if(semi1 || invert)
-    f(ctx, x, y, r, true, flip, semi1);
-  if(semi2 || invert)
-    f(ctx, x1, y1, r, true, flip, semi2);
-}
-
-var generateCodeSequence = function(xlow, xhigh, ylow, yhigh, r) {
-  var codes = [];
-  var count = 0;
-  for(var x = xlow; x <= xhigh; x+=r) {
-    for(var y = ylow; y <= yhigh; y+=r) {
-//      var low = 1 + (Math.floor(count + Math.random()/2 - 0.25) % 3);
-      var low = Math.floor(count + Math.random()/2 - 0.25) % 4;
-      var hi = 4 * Math.floor(Math.random() * 8);
-      if(Math.random() > 0.35) {
-        low = 0;
-        hi = hi & 15;
-      }
-      if(Math.random() > 0.5) {
-        hi = hi & 15;
-      }
-
-      codes.push(hi | low);
-      count++;
-    }
-  }
-  return codes;
-};
-
-var drawCodes = function(ctx, xlow, xhigh, ylow, yhigh, codes, r) { 
-  var count = 0;
-  for(var x = xlow; x <= xhigh; x+=r) {
-    for(var y = ylow; y <= yhigh; y+=r) {
-     drawBlock(ctx, x, y, r / 2, codes[count % codes.length]);
-     count++;
-    }
-  }
-};
-
-var drawCodesProb = function(ctx, xlow, xhigh, ylow, yhigh, codes, r, p) { 
-  var count = 0;
-  for(var x = xlow; x <= xhigh; x+=r) {
-    for(var y = ylow; y <= yhigh; y+=r) {
-     if(Math.random() > p) {
-       count++;
-       continue;
-     }
-     drawBlock(ctx, x, y, r / 2, codes[count % codes.length]);
-     count++;
-    }
-  }
-};
-
-var rgba = function(r, g, b, a) {
-  r = Math.floor(r);
-  g = Math.floor(g);
-  b = Math.floor(b);
-  var color = "rgba(" + [r,g,b,a].join(",") + ")";
-  window.console.log(color);
-  return color
-};
-
 subcanvas.withContext(function(ctx) {
-  var fill = rgba(0, 0, 0, 1);
-  ctx.fillStyle = fill;
-  var codes = generateCodeSequence(-size, size, -size * yOverX, size * yOverX, 1);
+  for(var reps = 0; reps < 10; reps++) {
+    var curve = new ble.curves.Kappa(
+      new goog.math.Vec2((Math.random()-0.5) * size, (Math.random()-0.5) * size),
+      Math.floor(Math.random() * 4) / 4 * 2 * Math.PI,
+      []);
+    curve.deltaAngle = ble.curves.ARCMINUTE;
 
-  ctx.beginPath();
-  drawCodes(ctx, -size, size, -(1 + size * yOverX), 1 + size * yOverX, codes, 1);
-  ctx.strokeStyle = "rgb(255,0,0)";
-  ctx.lineWidth *= 8;
-  ctx.stroke(); 
-  ctx.fill();
-  ctx.strokeStyle = "rgb(255,255,255)";
-  ctx.lineWidth /= 2;
-  ctx.stroke(); 
-  ctx.strokeStyle = "rgb(0,0,0)";
-  ctx.lineWidth /= 2;
-  ctx.stroke(); 
-
-  var codes = generateCodeSequence(-size, size, -size * yOverX, size * yOverX, 1/2);
-
-  ctx.beginPath();
-  drawCodes(ctx, -size, size, -(1 + size * yOverX), 1 + size * yOverX, codes, 1/2);
-  ctx.strokeStyle = "rgb(255,0,0)";
-  ctx.lineWidth *= 4;
-  ctx.stroke(); 
-  ctx.fill();
-  ctx.strokeStyle = "rgb(255,255,255)";
-  ctx.lineWidth /= 2;
-  ctx.stroke(); 
-  ctx.strokeStyle = "rgb(0,0,0)";
-  ctx.lineWidth /= 2;
-  ctx.stroke(); 
-
-  var codes = generateCodeSequence(-size, size, -size * yOverX, size * yOverX, 1/3);
-
-  ctx.beginPath();
-  drawCodes(ctx, -size, size, -(1 + size * yOverX), 1 + size * yOverX, codes, 1/3);
-  ctx.strokeStyle = "rgb(255,0,0)";
-  ctx.lineWidth *= 4;
-  ctx.stroke(); 
-  ctx.fill();
-  ctx.strokeStyle = "rgb(255,255,255)";
-  ctx.lineWidth /= 2;
-  ctx.stroke(); 
-  ctx.strokeStyle = "rgb(0,0,0)";
-  ctx.lineWidth /= 2;
-  ctx.stroke(); 
-
+    for(var i = 0; i < 200; i++)
+    {
+      if(Math.random() <= 0.5) {
+        curve.curve.push(new ble.curves.CurvedPart(Math.PI / 4, 2))
+      } else if(Math.random() <= 0.5) {
+        curve.curve.push(
+            new ble.curves.Angle(Math.PI / 2 * (Math.random() <= 0.5 ? -1 : 1))); 
+      } else {
+        curve.curve.push(
+            new ble.curves.Angle(Math.PI / 2 * (Math.random() <= 0.5 ? -1 : 1)),
+            new ble.curves.CurvedPart(1 / 2, 0));
+      }
+    }
+    curve.draw(ctx);
+  }
 });
